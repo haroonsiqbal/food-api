030ed7b4f221fec7a6aae463e9f31508d42d8513
Haroon Iqbal <harooniqbal@Haroons-MacBook-Pro.local>
2019-07-23 14:18:26 -0500

    added ingredients

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,16 +1,32 @@
 const foodLocation = document.querySelector(".foodList");
 
 const foodFetcher = () => {
-    fetch("http://localhost:8088/foods")
+fetch("http://localhost:8088/foods")
+    .then(response => response.json())
+    .then(myParsedFoods => {
+        myParsedFoods.forEach(food => {
+            console.log(food)
+
+    fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
     .then(foods => foods.json())
-    .then(parsedFoods => {
-        parsedFoods.forEach(food => {
+    .then(productInfo => {
+        console.log(productInfo);
+        if (productInfo.product.ingredients_text) {
+            food.ingredients = productInfo.product.ingredients_text
+        } else {
+            food.ingredients = "no ingredients listed"
+        }
+            food.countries = productInfo.product.countries
+            food.energy = productInfo.product.nutriments.energy
+            food.fat = productInfo.product.nutriments.fat
+            food.sugar = productInfo.product.nutriments.sugars
+
             const converted = foodAsHTML(food);
             foodFactory(converted);
         });
     });
-};
-
+})
+}
 
 const foodAsHTML = (foodObject) => {
     return `
@@ -18,6 +34,11 @@ const foodAsHTML = (foodObject) => {
     <h1>${foodObject.name}</h1>
     <h2>${foodObject.category}</h2>
     <h2>${foodObject.ethnicity}</h2>
+    <h2>${foodObject.ingredients}</h2>
+    <h2>${foodObject.countries}</h2>
+    <h2>${foodObject.energy} kcals per serving</h2>
+    <h2>${foodObject.fat} grams fat per serving</h2>
+    <h2>${foodObject.sugar} grams sugar per serving</h2>
     </section>
     `;
 };
